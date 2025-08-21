6934d84b94ad549072f448e86bb27902c89a8647
Isays Solis <isaysmail@gmail.com>
2025-08-21 11:30:34 -0600

    firstcommit

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import json
 
 app = Flask(__name__)
 
-
 CORS(app, resources={r"/*":{'origins':"*"}})
 #CORS(app, resources={r'/*':{'origins':'http://localhost:8080', "allow_headers": "Access-Control-Allow-Origin"}})
 
